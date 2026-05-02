b11b479306c509e1cdf5cba496ff30f855ce2617
Maxemelianum <maxemelianum1@gmail.com>
2026-05-02 12:23:44 +0300

    new simple console version

--- main/rent_calc_0.11.py
+++ main/rent_calc_0.11.py
@@ -1,1 +1,18 @@
-print("Hello I`m Rent Calculator!")
+value1 = float(input())
+value2 = float(input())
+value3 = float(input())
+
+ex_value1 = float(input())
+ex_value2 = float(input())
+ex_value3 = float(input())
+
+cost1 = float(input())
+cost2 = float(input())
+cost3 = float(input())
+
+res1 = (value1 - ex_value1) * cost1
+res2 = (value2 - ex_value2) * cost2
+res3 = (value3 - ex_value3) * cost3
+
+
+print("Result #1", res1, "\n", "Result #2", res2, "\n" "Result #3", res3)
